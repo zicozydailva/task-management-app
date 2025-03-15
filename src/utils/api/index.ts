297569c6d15e865fb3ef5ccf3d93a4fc57bc101c
Ogbu Ezekiel <zicozydasilva70@gmail.com>
2025-03-15 13:03:56 +0100

    chore: dynamic tokens

--- src/utils/api/index.ts
+++ src/utils/api/index.ts
@@ -3,7 +3,6 @@ import axios from "axios";
 const BASE_API_URL = import.meta.env.VITE_BASE_URL;
 
 const useAxiosInstance = (multipart = false) => {
-  // Create the axios instance
   const instance = axios.create({
     headers: {
       "Content-Type": multipart ? "multipart/form-data" : "application/json",
@@ -12,13 +11,9 @@ const useAxiosInstance = (multipart = false) => {
     timeout: 60000,
   });
 
-  // Add a request interceptor to set the auth token
   instance.interceptors.request.use(
     async (config) => {
       try {
-        // const token =
-        //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2N2Q0NmEyNDFkYjhiMjM1ZGNmODIyNDciLCJmaXJzdE5hbWUiOiJKb2huIiwibGFzdE5hbWUiOiJEb2UiLCJlbWFpbCI6ImpvaG5AZ21haWwuY29tIiwicGFzc3dvcmQiOiIkMmIkMTAkdGxaYlVLak9PSU1kMUhGT1EyT2RNLlNqbEZ5cXFkTmR5cG1lOGhyRmlaaThDaG5SWlBPVWEiLCJjcmVhdGVkQXQiOiIyMDI1LTAzLTE0VDE3OjQwOjUyLjQzOFoiLCJ1cGRhdGVkQXQiOiIyMDI1LTAzLTE0VDE3OjQyOjU2Ljk1MVoiLCJfX3YiOjAsInNlc3Npb25JZCI6IkRBMXl0dnFRUVFiUFB4UzdhMzhMRiIsImlhdCI6MTc0MTk3OTcxMCwiZXhwIjoxNzQzMTg5MzEwfQ.suuBROYFur3DrXOWvvVFP5_5owsO63WFFhPTfzsKdOc";
-
         const token = localStorage.getItem("accessToken");
 
         config.headers.Authorization = `Bearer ${token}`;
