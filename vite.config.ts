7714697648e7f136fb4a133e45b86d9e96fc2c62
Ogbu Ezekiel <zicozydasilva70@gmail.com>
2025-03-16 15:34:42 +0100

    chore: render refresh fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {},
+  build: {
+    outDir: "dist",
+  },
+  base: "/", // Ensure proper routing on deployment
 });
